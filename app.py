307549cf53359b37ea0d26c01d8a4ae137508627
ToshnoWor <47005196+ToshnoWor@users.noreply.github.com>
2023-07-21 16:01:23 +0000

    added dynamic pages for each person, added 404 page, create copypaste

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, jsonify
-from database import load_person_from_db
+from database import load_persons_from_db, load_person_from_db
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  persons = load_person_from_db()
+  persons = load_persons_from_db()
   return render_template('home.html', persons=persons)
 
 
 @app.route("/api/persons")
 def list_name():
-  persons = load_person_from_db()
+  persons = load_persons_from_db()
   return jsonify(persons)
+
+
+@app.route("/person/<id>")
+def show_person(id):
+  person = load_person_from_db(id)
+  if not person:
+    err_messame = "Not found person with id: " + id
+    return render_template('404.html', message=err_messame), 404
+  return render_template('personpage.html', person=person)
+  # return jsonify(person)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,18 +1,30 @@
 import os
 from sqlalchemy import create_engine, text
 
-
 my_secret = os.environ['DB_CONNECTION_STRING']
 db_connection_string = my_secret
 
 connect_args = {"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
 
-engine = create_engine(db_connection_string, connect_args=connect_args, echo=False)
+engine = create_engine(db_connection_string,
+                       connect_args=connect_args,
+                       echo=False)
 
-def load_person_from_db():
+
+def load_persons_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from person"))
     persons = []
     for row in result.mappings():
       persons.append(dict(row))
     return persons
+
+
+def load_person_from_db(id):
+  s = text("select * from person where id = :id")
+  id_dict = dict(id=id)
+  with engine.connect() as conn:
+    result = conn.execute(s, parameters=id_dict)
+    if result.rowcount == 0:
+      return None
+    return dict(result.mappings().all()[0])
